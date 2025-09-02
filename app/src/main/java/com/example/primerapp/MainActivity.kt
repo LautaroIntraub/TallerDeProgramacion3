28664cb9228103ac428f16e51e5ab187fb4fee56
Lautaro Intraub <lautarointraub@hotmail.com>
2025-09-02 19:01:53 -0300

    Estructura

---
+++ app/src/main/java/com/example/primerapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.primerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.primerapp.ui.theme.PrimerAppTheme
+import androidx.compose.material3.Button
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrimerAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Button(onClick = {}, modifier = modifier) {
+        Text(text = "Hello $name!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrimerAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/primerapp/ui/screens/WelcomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.primerapp.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun welcomeScreen() {
+
+}
